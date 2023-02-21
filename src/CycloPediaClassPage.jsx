c51a995e470b6d242ee13651a1c22cca741d37f8
Nirav Soni <soninirav910@gmail.com>
2023-02-21 19:15:27 +0530

    componentDidMount()

--- src/CycloPediaClassPage.jsx
+++ src/CycloPediaClassPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { getRandomUser } from "./Utility/api";
+
 class CycloPediaClassPage extends React.Component{
     constructor(props){
         super(props);
@@ -11,7 +13,47 @@ class CycloPediaClassPage extends React.Component{
         };
     }
 
+    componentDidMount = async() => {
+        console.log('Component Did Mount');
+        const response = await getRandomUser();
+        console.log(response);
+
+        this.setState((prevState) => {
+            return {
+                instructor: {
+                    name: response.data.first_name + " " + response.data.last_name,
+                    email: response.data.email,
+                    phone: response.data.phone_number,
+                },
+            };
+        });
+    }
+
+    componentDidUpdate(){
+        console.log('Component Did Update');
+    }
+
+    componentWillUnmount(){
+        console.log('Component Will Unmount');
+    }
+
     render(){
-        
+        console.log('Render component');
+        return (<div>
+            {this.state.instructor && (
+            <div className="p-3">
+                <span className="h4 text-success">Instructor</span>
+                <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+                <br />
+                Name: {this.state.instructor.name}
+                <br />
+                Email: {this.state.instructor.email}
+                <br />
+                Phone: {this.state.instructor.phone}
+            </div>)
+            }
+        </div>)
     }
 }
+
+export default CycloPediaClassPage; 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CycloPediaClassPage from './CycloPediaClassPage';
 import Header from './Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header />
+  <div>
+    <Header />
+    <div className="row text-white">
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Class Component</span>
+        <CycloPediaClassPage></CycloPediaClassPage>
+      </div>
+    </div>
+  </div>
 );
